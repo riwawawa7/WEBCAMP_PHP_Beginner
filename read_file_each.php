581b798ef4a852c0dd2dc173b8e05a3005c155a4
EC2 Default User <ec2-user@ip-172-31-0-252.ap-northeast-1.compute.internal>
2023-04-26 01:08:53 +0000

    read_file_each.php read_file_all.php を書いた

---
+++ read_file_each.php
@@ -0,0 +1,12 @@
+<?php
+//ファイル名の把握
+$file_name = __FILE__;
+
+//ファイルのopen
+$file_obj = new Splfileobject($file_name);
+
+//各行を読み込んで出力する
+foreach($file_obj as $no => $line){
+    echo"{$no}: $line";
+
+}
