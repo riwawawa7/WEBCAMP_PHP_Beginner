d1be3b39c0be9166546d40a95bc6b6352a13a313
EC2 Default User <ec2-user@ip-172-31-0-252.ap-northeast-1.compute.internal>
2023-05-01 05:24:10 +0000

    fibonacci.phpの提出課題2回目

--- PHP_kadai/fibonacci_02.php
+++ PHP_kadai/fibonacci_02.php
@@ -9,9 +9,13 @@ $b = 1;
 while ($b <= 10000){
     $c = $a + $b;
     echo "{$c} \n";
-    $a = $b + $c;
-    echo "{$a} \n";
-    $b = $a + $c;
-    echo "{$b} \n";
 
+    $a = $b + $c;
+    if ($c <= 10000){
+    echo "{$a} \n";
+    }
+    $b = $c + $a;
+    if ($a <= 10000){
+    echo "{$b} \n";
+    }
 }
